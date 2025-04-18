41a94cf67ac77fded4d86273163085ce23d05ce8
KajiyamaYusuke <y.kajiyama@biomech.me.es.osaka-u.ac.jp>
2025-04-18 16:18:30 +0900

    2.3

--- include/Poisson.h
+++ include/Poisson.h
@@ -15,16 +15,15 @@ public:
     void analyze_node();//読み込んだ情報からdx,dy,Lx,Lyを出す
     void makeMatrix();//形状と境界条件から係数行列とベクトルを作る
     void solver();//連立方程式を解く
-    void output_phi();//出力
-    void showMap();
+    void output_phi();//出力(確認用)
+    void showMap();//描画
 
 private:
-    std::string message_;//非公開メンバ
+    //非公開メンバ
     std::string node_path;
     
     std::vector<std::pair<double, double>> nodes;// ノード情報（x, y）を格納
 
-    double length ;
     double dx, dy ;
     double Lx, Ly ;
  
