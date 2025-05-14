a60bd43a534e7dc07f97d5cec17149240576275f
ikeshima254 <ayato@Ayato.>
2025-05-15 02:39:49 +0900

    submit

--- src/OutputCSV.cpp
+++ src/OutputCSV.cpp
@@ -9,6 +9,10 @@ using namespace std;
 
 void OutputCSV::output(const std::vector<double>& x, const std::string& filename){
 
+    const int Nx = 10;
+    const int Ny = 10;
+    const double dx = 0.01;
+
     std::ofstream file(filename);
     
     if(!file){
@@ -16,8 +20,14 @@ void OutputCSV::output(const std::vector<double>& x, const std::string& filename
         return;
     }
 
-    for (double val : x) {
-        file << val << "\n";
+    for (int j = 0; j < Ny; ++j) {
+        for (int i = 0; i < Nx; ++i) {
+            int idx = j * Nx + i;
+            double x_coord = i * dx;
+            double y_coord = j * dx;
+
+            file << "(" << x_coord << "," << y_coord << "), " << x[idx] << "\n";
+        }
     }
 
     file.close();
